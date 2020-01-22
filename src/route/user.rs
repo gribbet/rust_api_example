3ebfa0b4519069b628e1b0f20bdb3ec93b2a64b5
Graham <graham.gibbons@gmail.com>
2020-01-21 21:35:54 -0800

    Cleanup

--- src/route/user.rs
+++ src/route/user.rs
@@ -4,12 +4,13 @@ use crate::model::user::User;
 use crate::service::user;
 use rocket::State;
 use rocket_contrib::json::Json;
+use std::ops::Deref;
 
 #[get("/")]
 pub async fn list(
     database: State<'_, Database>,
 ) -> Result<Json<Vec<User>>, ApiError> {
-    Ok(Json(user::list(&*&database.get()?)?))
+    Ok(Json(user::list(database.get()?.deref())?))
 }
 
 #[get("/<id>")]
@@ -17,7 +18,7 @@ pub async fn get(
     database: State<'_, Database>,
     id: i32,
 ) -> Result<Option<Json<User>>, ApiError> {
-    Ok(user::get(&*&database.get()?, id)?.map(|user| Json(user)))
+    Ok(user::get(database.get()?.deref(), id)?.map(|user| Json(user)))
 }
 
 #[post("/", format = "json", data = "<user>")]
@@ -25,5 +26,8 @@ pub async fn create(
     database: State<'_, Database>,
     user: Json<User>,
 ) -> Result<Json<User>, ApiError> {
-    Ok(Json(user::create(&*&database.get()?, user.into_inner())?))
+    Ok(Json(user::create(
+        database.get()?.deref(),
+        user.into_inner(),
+    )?))
 }
