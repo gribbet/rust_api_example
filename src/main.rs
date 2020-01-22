547c668315a0957bba351d4724a18f7872b82ca8
Graham <graham.gibbons@gmail.com>
2020-01-21 17:18:13 -0800

    JSON

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,36 @@
 
 #[macro_use]
 extern crate rocket;
+extern crate rocket_contrib;
+#[macro_use]
+extern crate serde_derive;
+
+use rocket_contrib::json::Json;
+use std::process;
+
+#[derive(Serialize, Deserialize)]
+struct User {
+    id: u32,
+    name: String,
+}
 
 #[get("/")]
-async fn index() -> String {
-    "Hello, world!".to_string()
+fn get() -> Json<User> {
+    let user = User {
+        id: 1,
+        name: String::from("Testy"),
+    };
+    Json(user)
+}
+
+#[post("/", format = "json", data = "<user>")]
+fn post(user: Json<User>) -> Json<User> {
+    user
 }
 
 fn main() {
-    rocket::ignite()
-        .mount("/", routes![index])
-        .launch()
-        .unwrap();
+    if let Err(error) = rocket::ignite().mount("/", routes![get, post]).launch() {
+        println!("Error: {}", error);
+        process::exit(1)
+    }
 }
